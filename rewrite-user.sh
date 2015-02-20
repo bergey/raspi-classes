ba40b2675edf7467659d6b76f75c59138d4f9399
Daniel Bergey <bergey@alum.mit.edu>
2015-02-20 17:11:43 +0000

    add header comment

--- rewrite-user.sh
+++ rewrite-user.sh
@@ -1,3 +1,6 @@
+# prepare the intro for printing, by making a custom copy for each user
+# one page per user, with a different username filled in for each
+# call pandoc on the resulting .md file
 set -x
 rm intro-ssh-raspi-multiple.md
 for user in fluttershy blossomforth cloudchaser thunderlane stellar-eclipse soarin whiplash bluebell; do
