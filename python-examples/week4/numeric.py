9d40764653e772abddc41ffd2ab03fe7cdd98b17
Daniel Bergey <bergey@alum.mit.edu>
2015-03-16 17:35:37 +0000

    week 4 handout, code excercises

---
+++ python-examples/week4/numeric.py
@@ -0,0 +1,19 @@
+print("Enter a number between 1 and 100")
+x = input()
+if x ?? 100:
+    print("That number is too big!")
+elif x ?? 1:
+    print("That number is too small!")
+else:
+    print("{} is a good number.".format(x))
+
+print("Enter a number between 7 and 72.")
+y = input()
+if y ?? ??:
+    print("That number is too big!")
+elif y ?? ??:
+    print("That number is too small!")
+else:
+    print("{} is a good number.".format(y))
+
+
