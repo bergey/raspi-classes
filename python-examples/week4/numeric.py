2e1ea4de6934347fe494699c67d436bfd79aef3d
Daniel Bergey <bergey@alum.mit.edu>
2015-03-19 14:47:45 +0000

    code listings in monospace font, fixes quote marks

--- python-examples/week4/numeric.py
+++ python-examples/week4/numeric.py
@@ -6,14 +6,3 @@ elif x ?? 1:
     print("That number is too small!")
 else:
     print("{} is a good number.".format(x))
-
-print("Enter a number between 7 and 72.")
-y = input()
-if y ?? ??:
-    print("That number is too big!")
-elif y ?? ??:
-    print("That number is too small!")
-else:
-    print("{} is a good number.".format(y))
-
-
